f97d45298811fae8a465288f3e51ee6e55ae892a
porschefx9 <65465001+porschefx9@users.noreply.github.com>
2020-05-16 20:04:57 -0500

    SECOND TIME SAVING

--- main.ts
+++ main.ts
@@ -24,15 +24,24 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         mySprite.vy = -200
     }
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
+    game.over(false)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
+    game.over(true, effects.blizzard)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+    game.over(false)
+})
 let mySprite: Sprite = null
 tiles.setTilemap(tiles.createTilemap(
-            hex`10001000040b100b0b0b100b0b0b100b0b0b100a0100001500150015001500090000000901000015000000000015000900000009010000000000000000000009000000090100000000090009000000090000000901000009000900090009000900000009010b100b0b0b100b0b0b100b000b100b0100000000000000000000000000000901000000000000000000000000000009010b00000b0b0b0a000000030303030801000000000000020a000000000000000100000000000000100a00000000000001000e000000000000020a000000000001070c0707070707070709000000000001070c0707070711070709060506050602030303030303030303080506050605`,
+            hex`10001000040b100b0b0b100b0b0b100b0b0b100a0100001500150015001500000000000901000015000000000015000000000009010000000000000000000009000000090100000000090009000000090000000901000009000900090009000900000009010b100b0b0b100b0b0b100b000b100b0100000000000000000000000000000901000000000000000000000000000009010b00000b0b0b0a000000030303030801000000000000020a000000000000000100000000000000100a000000000000010000000000000e00020a0000000000010707070707071807070900000000000107070707070718070709060506050602030303030303030303080506050605`,
             img`
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 . . 2 . 2 . 2 . 2 . 2 . . . 2 
-2 . . 2 . . . . . 2 . 2 . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . 2 . 2 . . . . . . . 2 
+2 . . 2 . 2 . 2 . 2 . . . . . 2 
+2 . . 2 . . . . . 2 . . . . . 2 
+2 . . . . . . . . . . 2 . . . 2 
+2 . . . . 2 . 2 . . . 2 . . . 2 
 2 . . 2 . 2 . 2 . 2 . 2 . . . 2 
 2 2 2 2 2 2 2 2 2 2 2 2 . 2 2 2 
 2 . . . . . . . . . . . . . . 2 
@@ -41,11 +50,11 @@ tiles.setTilemap(tiles.createTilemap(
 2 . . . . . . 2 2 . . . . . . . 
 2 . . . . . . . 2 2 . . . . . . 
 2 . . . . . . . . 2 2 . . . . . 
-2 . 2 . . . . . . . 2 . . . . . 
-2 . 2 . . . . . . . 2 . . . . . 
+2 . . . . . . 2 . . 2 . . . . . 
+2 . . . . . . 2 . . 2 . . . . . 
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 `,
-            [myTiles.tile0,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.hazardLava0,sprites.dungeon.hazardLava1,sprites.dungeon.hazardWater,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterNorth0,sprites.dungeon.floorLight4,sprites.dungeon.chestClosed,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.collectibleInsignia,sprites.dungeon.greenOuterNorth2,sprites.builtin.coral5,sprites.builtin.oceanSand3,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterEast0,sprites.dungeon.floorMixed,sprites.dungeon.greenOuterEast2],
+            [myTiles.tile0,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.hazardLava0,sprites.dungeon.hazardLava1,sprites.dungeon.hazardWater,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterNorth0,sprites.dungeon.floorLight4,sprites.dungeon.chestClosed,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.collectibleInsignia,sprites.dungeon.greenOuterNorth2,sprites.builtin.coral5,sprites.builtin.oceanSand3,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterEast0,sprites.dungeon.floorMixed,sprites.dungeon.greenOuterEast2,sprites.dungeon.floorLight0],
             TileScale.Sixteen
         ))
 mySprite = sprites.create(img`
@@ -69,8 +78,9 @@ c 5 5 5 5 c 5 5 5 5 c 4 c 5 c .
 controller.moveSprite(mySprite)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 5))
 mySprite.ay = 500
-music.playMelody("G E B D F A C C5 ", 120)
+music.playMelody("A E G F A C5 C G ", 120)
+mySprite.say("Please help me get to the end")
 game.onUpdateInterval(2000, function () {
-    let projectile: Sprite = null
-    tiles.placeOnTile(projectile, tiles.getTileLocation(0, 0))
+    scene.cameraFollowSprite(mySprite)
+    info.changeScoreBy(1)
 })
